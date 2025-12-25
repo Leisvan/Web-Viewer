9253591155b1dc738a3f55209a6c722df3614bc1
Leisvan Cordero <rathma@live.com>
2025-12-24 19:37:29 -0600

    Refactor to WebViewer namespace, add WebPreviewPage

--- Web Viewer/MainWindow.xaml.cs
+++ Web Viewer/MainWindow.xaml.cs
@@ -12,20 +12,14 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinUIEx;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace WebViewer;
 
-namespace Web_Viewer
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
